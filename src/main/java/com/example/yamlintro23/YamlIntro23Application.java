68707be8b533d67352f51e4e712ce7db31d878e4
navidmirzad <navi0221@stud.kea.dk>
2023-08-31 15:06:04 +0200

    first commit YAML-Intro

---
+++ src/main/java/com/example/yamlintro23/YamlIntro23Application.java
@@ -0,0 +1,13 @@
+package com.example.yamlintro23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlIntro23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamlIntro23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yamlintro23/YamlIntro23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yamlintro23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlIntro23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
